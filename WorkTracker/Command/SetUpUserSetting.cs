d3a54aea24de32b5fe9f5fca7bfa6fe10c775065
Lu_Cao <zergmk2@gmail.com>
2016-12-23 17:06:20 +0800

    [Close #7] : Mask password inputted in console.

--- WorkTracker/Command/SetUpUserSetting.cs
+++ WorkTracker/Command/SetUpUserSetting.cs
@@ -22,13 +22,53 @@ namespace WorkTracker.Command
                 Console.WriteLine(string.Format("Please input the user name of Jira Server {0} : ", userSetting.JiraServerAddress));
                 userSetting.JiraUserName = Console.ReadLine().Trim();
                 Console.WriteLine(string.Format("Please input the password of Jira Server {0} : ", userSetting.JiraServerAddress));
-                userSetting.JiraPassword = Console.ReadLine().Trim();
+                ConsoleKeyInfo key;
+                var sb = new StringBuilder();
+                do
+                {
+                    key = Console.ReadKey(true);
+                    if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                    {
+                        sb.Append(key.KeyChar);
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        if (key.Key == ConsoleKey.Backspace && sb.Length > 0)
+                        {
+                            sb.Remove(sb.Length - 1, 1);
+                            Console.Write("\b \b");
+                        }
+                    }
+                } while (key.Key != ConsoleKey.Enter);
+                userSetting.JiraPassword = sb.ToString();
+                sb.Clear();
+                Console.Write(Environment.NewLine);
+
                 Console.WriteLine(string.Format(@"Please input the Proxy server URL, for example : http://proxy.server.com:80 "));
                 userSetting.ProxyServer = Console.ReadLine().Trim();
                 Console.WriteLine(string.Format("Please input the user name of Proxy Server {0} : ", userSetting.ProxyServer));
                 userSetting.ProxyUserName = Console.ReadLine().Trim();
                 Console.WriteLine(string.Format("Please input the password of Proxy Server {0} : ", userSetting.ProxyServer));
-                userSetting.ProxyPassword = Console.ReadLine().Trim();
+                do
+                {
+                    key = Console.ReadKey(true);
+                    if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                    {
+                        sb.Append(key.KeyChar);
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        if (key.Key == ConsoleKey.Backspace && sb.Length > 0)
+                        {
+                            sb.Remove(sb.Length - 1, 1);
+                            Console.Write("\b \b");
+                        }
+                    }
+                } while (key.Key != ConsoleKey.Enter);
+                Console.WriteLine(Environment.NewLine);
+                userSetting.ProxyPassword = sb.ToString();
 
                 using (var memoryStream = new MemoryStream())
                 {
